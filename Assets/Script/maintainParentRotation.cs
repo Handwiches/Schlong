2059090a33d1b9b572b13f78524c2700b935e8b9
Handwiches <dillon.sommerville@yahoo.com>
2016-07-09 14:36:40 -0400

    Fixed particles (for real this time)
    blops

--- Assets/Script/maintainParentRotation.cs
+++ Assets/Script/maintainParentRotation.cs
@@ -12,6 +12,6 @@ public class maintainParentRotation : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        transform.rotation = Quaternion.Euler(new Vector3(-90, 0, transform.parent.rotation.z));
+        transform.rotation = Quaternion.Euler(new Vector3((transform.parent.eulerAngles.x + 180.0f), (transform.parent.eulerAngles.y), transform.parent.eulerAngles.z)); //Quaternion.Euler(new Vector3(-90, 0, transform.parent.rotation.z));
 	}
 }
